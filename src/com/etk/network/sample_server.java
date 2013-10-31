9efb319d0312ef77359a5a0706257833484f5cd7
supervitis <supervitis@hotmail.es>
2013-10-31 02:10:28 +0100

    Test 2

--- src/com/etk/network/sample_server.java
+++ src/com/etk/network/sample_server.java
@@ -2,15 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.security.*;
 
-/**
- * Title:        Sample Server
- * Description:  This utility will accept input from a socket, posting back to the socket before closing the link.
- * It is intended as a template for coders to base servers on. Please report bugs to brad at kieser.net
- * Copyright:    Copyright (c) 2002
- * Company:      Kieser.net
- * @author B. Kieser
- * @version 1.0
- */
+
 
 public class sample_server {
 
