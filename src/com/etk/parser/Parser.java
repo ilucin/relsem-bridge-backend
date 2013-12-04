91699c769e024cbcc39ef5fffa26a529453ba567
Michele Castellana <blacklion727@gmail.com>
2013-12-04 16:49:20 +0100

    Example interface for the parser (needs to be completed)

---
+++ src/com/etk/parser/Parser.java
@@ -0,0 +1,16 @@
+package com.etk.parser;
+
+public interface Parser {
+	/*
+	 * Put definition of the methods we have to use here and please, put also
+	 * javadocs to understand what methods do and what paremeters are used to.
+	 * For instance:
+	 */
+	/**
+	 * this method do this, this, and this
+	 * 
+	 * @param b
+	 *            represent the integer to do this this and this
+	 */
+	public void foo(int b);
+}
