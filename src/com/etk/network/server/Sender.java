a35b29a81ed80ed20467359f1a9463a163a42b2f
Michele Castellana <Espanico@users.noreply.github.com>
2013-12-07 19:36:51 +0100

    FIX: compiler errors

--- src/com/etk/network/server/Sender.java
+++ src/com/etk/network/server/Sender.java
@@ -4,6 +4,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Sender {
 
@@ -454,7 +455,6 @@ public class Sender {
 				 * = Base64.encodeBase64String(salt);
 				 */
 				throw new NotImplementedException();
-				break;
 			}
 		} catch (IOException e) {
 			System.out.println("Error in sendAuthenticationOkMessage: ");
