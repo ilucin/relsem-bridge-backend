23cbe934a94980273b09222d2d2a2348c341c474
Michele Castellana <Espanico@users.noreply.github.com>
2013-12-16 12:40:39 +0100

    Better management of the the input in the MsgParser

--- src/com/etk/network/server/MsgParser.java
+++ src/com/etk/network/server/MsgParser.java
@@ -2,32 +2,37 @@ package com.etk.network.server;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 
 public class MsgParser {
 
-	public MsgParser(){
-		
+	public MsgParser() {
 	}
-	
-	public short parseShort(byte[] bytes) {
-		byte[] typeBytes = new byte[2];
-		typeBytes[0] = 0;
-		typeBytes[1] = bytes[0];
-		return ByteBuffer.wrap(typeBytes).getShort();
+
+	public short parseShort(byte[] bytes) throws IllegalArgumentException {
+		if (bytes.length < 2)
+			throw new IllegalArgumentException(
+					"The array of bytes which represent the short int has less than 2 bytes");
+		if (bytes.length > 2)
+			throw new IllegalArgumentException(
+					"The array of bytes which represent the short int has more than 2 bytes");
+		return ByteBuffer.wrap(bytes).getShort();
 
 	}
 
-	public int parseInt(byte[] bytes) {
-		byte[] lenBytes = Arrays.copyOf(bytes, 4);
-		int len = ByteBuffer.wrap(lenBytes).getInt();
+	public int parseInt(byte[] bytes) throws IllegalArgumentException {
+		if (bytes.length < 4)
+			throw new IllegalArgumentException(
+					"The array of bytes which represent the integer has less than 4 bytes");
+		if (bytes.length > 4)
+			throw new IllegalArgumentException(
+					"The array of bytes which represent the integer has more than 4 bytes");
+		int len = ByteBuffer.wrap(bytes).getInt();
 		return len;
 	}
 
-	public String parseMsg(byte[] bytes)
-			throws UnsupportedEncodingException {
+	public String parseMsg(byte[] bytes) throws UnsupportedEncodingException {
 		String msgString = new String(bytes, "UTF-8");
 		return msgString;
 	}
-	
+
 }
