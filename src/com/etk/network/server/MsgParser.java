8c7cdeab219a61713737341bda1e17b29ba855f4
Michele Castellana <Espanico@users.noreply.github.com>
2013-12-27 19:02:33 +0100

    Javadocs for the MsgParser class

--- src/com/etk/network/server/MsgParser.java
+++ src/com/etk/network/server/MsgParser.java
@@ -3,11 +3,27 @@ package com.etk.network.server;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 
+/**
+ * An helper class to manage the bytes cast into other formats (int, short int,
+ * string)
+ * 
+ * @author Michele
+ * 
+ */
 public class MsgParser {
-
+	
 	public MsgParser() {
 	}
 
+	/**
+	 * Take a 2 bytes lenght array and returns the corrisponding short int
+	 * 
+	 * @param bytes
+	 *            The bytes that represent the short int
+	 * @return The short int number
+	 * @throws IllegalArgumentException
+	 *             iff the input has less than 2 bytes or more than 2 bytes
+	 */
 	public short parseShort(byte[] bytes) throws IllegalArgumentException {
 		if (bytes.length < 2)
 			throw new IllegalArgumentException(
@@ -19,6 +35,15 @@ public class MsgParser {
 
 	}
 
+	/**
+	 * Take a 4 bytes lenght array and returns the corrisponding int
+	 * 
+	 * @param bytes
+	 *            The bytes that represent the int
+	 * @return The int number
+	 * @throws IllegalArgumentException
+	 *             iff the input has less than 4 bytes or more than 4 bytes
+	 */
 	public int parseInt(byte[] bytes) throws IllegalArgumentException {
 		if (bytes.length < 4)
 			throw new IllegalArgumentException(
@@ -29,7 +54,16 @@ public class MsgParser {
 		int len = ByteBuffer.wrap(bytes).getInt();
 		return len;
 	}
-
+	
+	/**
+	 * Take an array of bytes and returns the corrisponding string
+	 * 
+	 * @param bytes
+	 *            The bytes that represent the string
+	 * @return The string
+	 * @throws UnsupportedEncodingException
+	 *             iff the named charset is not supported
+	 */
 	public String parseMsg(byte[] bytes) throws UnsupportedEncodingException {
 		String msgString = new String(bytes, "UTF-8");
 		return msgString;
