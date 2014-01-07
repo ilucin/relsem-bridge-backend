5d6998bcf7974fba130812ae5d7d5a667ff3e9a5
David Riobo <supervitis@gmail.com>
2014-01-07 01:37:05 +0100

    Almost done

--- src/tests/ReceiverTest.java
+++ src/tests/ReceiverTest.java
@@ -4,56 +4,89 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 
+import javax.lang.model.type.NullType;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import com.etk.network.server.Receiver;
+import com.etk.network.server.Sender;
 
 public class ReceiverTest extends AbstractConnectionTest {
 
 	Receiver receiver;
-	
+	Sender sender;
+
 	@Before
 	public void setUp() throws Exception {
-	
+
 		this.receiver = new Receiver(dataInputStream);
-		
+
 	}
+
 	
-	
-	
+
+	@Test
+	public void testGetPassword() throws IOException {
+		dataOutputStream.writeInt(10);
+		dataOutputStream
+				.writeBytes(new String(nullTerminateString("postgres")));
+		dataOutputStream.flush();
+		this.receiver.getPassword();
+	}
+
+	@Test
+	public void testReceiveAuthMessage() throws IOException {
+
+		String string = "postgres";
+		int size = nullTerminateString(string).length;
+		dataOutputStream.writeInt(4 + 2 + 2 + 2 * size);
+		dataOutputStream.writeShort(9);
+		dataOutputStream.writeShort(2);
+		dataOutputStream
+				.writeBytes(new String(nullTerminateString("postgres")));
+		dataOutputStream
+				.writeBytes(new String(nullTerminateString("postgres")));
+		dataOutputStream.flush();
+		this.receiver.receiveAuthMessage();
+	}
+
 	@Test
 	public void testGetUsername() {
-		fail("Not yet implemented");
+		this.receiver.getUsername();
 	}
 
 	@Test
 	public void testGetDBName() {
-		fail("Not yet implemented");
+		this.receiver.getDBName();
 	}
-
-	@Test
-	public void testGetPassword() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testReceiveAuthMessage() {
-		fail("Not yet implemented");
-	}
-
+	
 	@Test
 	public void testGetMessageType() throws IOException {
-	
+
 		dataOutputStream.writeByte('E');
 		dataOutputStream.flush();
-		receiver.getMessageType();
-	
+		this.receiver.getMessageType();
+
 	}
 
 	@Test
-	public void testReadQueryMessage() {
-		fail("Not yet implemented");
+	public void testReadQueryMessage() throws IOException {
+		dataOutputStream.writeInt(10);
+		dataOutputStream.writeBytes(new String(nullTerminateString("SELECT host FROM festival;")));
+		dataOutputStream.flush();
+		this.receiver.readQueryMessage();
+		
+	}
+
+	private byte[] nullTerminateString(String string) {
+		byte[] in = string.getBytes();
+		byte[] out = new byte[in.length + 1];
+		for (int i = 0; i < in.length; i++) {
+			out[i] = in[i];
+		}
+		out[in.length] = '\0';
+		return out;
 	}
 
 }
